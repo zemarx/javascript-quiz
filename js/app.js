a2e0a3a059523b69a20b5a77e373af523e80b796
Mark <mark.piispanen@hotmail.com>
2016-10-15 23:34:12 +0300

    Modified html and js files

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ function Quiz() {
     //------------Private variables-------------------
 
     var currentQuestion = 0;
+    var points = 0;
+    var userAnswers = [];
 
     //-------------Public functions--------------------
 
@@ -13,18 +15,34 @@ function Quiz() {
     };
 
     this.nextQuestion = function () {
+
+        //check if answer is selected
+        //save users's answer
+        //check if the answer is right and set user's points
+
         currentQuestion++;
     };
 
     this.previousQuestion = function () {
-
+        //get user's answer by question number
+        //
     };
 
-    this.startAgain = function () {
+    this.initQuiz = function () {
+        currentQuestion = 0;
+        points = 0;
+    };
 
+    this.restartQuiz = function () {
+        // TODO:
     };
 
     //--------------Private functions------------------
+
+    function setCurrentQuestion () {
+        //get question from somewhere
+        //set choices
+    };
 
     function privateFun() {
         console.log("I am a private function: " + currentQuestion);
