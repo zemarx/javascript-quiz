d288317cc2c6191a413afd4107024ccb5c178906
Mark <mark.piispanen@hotmail.com>
2016-10-16 00:35:01 +0300

    Added json file with questions. Implemented method for getting json data

--- js/app.js
+++ js/app.js
@@ -2,23 +2,33 @@ function Quiz() {
 
     //------------Private variables-------------------
 
+    var questions = []; //arr of objects
     var currentQuestion = 0;
     var points = 0;
     var userAnswers = [];
+    var answerSelected = false; // or do it like a function which return bool
 
     //-------------Public functions--------------------
 
-    //DEBUG FUNCTION
-    this.printQuestionNum = function () {
-        console.log(currentQuestion);
-        privateFun();
+    this.startQuiz = function () {
+        console.log(JSON.stringify(questions, null, 2));
+    };
+
+    this.init = function () {
+        getData('GET', 'data/quiz-data.json', (function (err, data){
+            if (err) {
+                throw err;
+            }
+            questions = data;
+            this.startQuiz();
+        }).bind(this));
     };
 
     this.nextQuestion = function () {
-
-        //check if answer is selected
+        //check if answer is selected -> else return
         //save users's answer
         //check if the answer is right and set user's points
+        setCurrentQuestion();
 
         currentQuestion++;
     };
@@ -28,7 +38,7 @@ function Quiz() {
         //
     };
 
-    this.initQuiz = function () {
+    this.initQuiz = function (data) {
         currentQuestion = 0;
         points = 0;
     };
@@ -40,15 +50,35 @@ function Quiz() {
     //--------------Private functions------------------
 
     function setCurrentQuestion () {
-        //get question from somewhere
+        //clear radio buttons
+        //get question from somewhere by "currentQuestion"
         //set choices
     };
 
-    function privateFun() {
-        console.log("I am a private function: " + currentQuestion);
+    function getData(method, url, callback) {
+        var request = new XMLHttpRequest();
+        request.open(method, url, true);
+
+        request.onload = function() {
+            if (request.status >= 200 && request.status < 400) {
+                var data = JSON.parse(request.responseText);
+                callback(null, data);
+            } else {
+                // We reached our target server, but it returned an error
+            }
+        };
+
+        request.onerror = function() {
+            // There was a connection error of some sort
+            callback('Connection error', null);
+        };
+
+        request.send();
     };
 };
 
 var quiz = new Quiz();
+
+quiz.init();
 
 //set listeners to buttons
